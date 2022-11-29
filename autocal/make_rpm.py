eeeb67f3216d27c7c26c74ed7904e714a445ab22
Iyarr <8.24ridiculous@gmail.com>
2022-11-29 02:59:34 +0000

    変更なし

--- autocal/make_rpm.py
+++ autocal/make_rpm.py
@@ -1,90 +1,97 @@
 class Method:
-#   逆ポーランド記法への変換
     def convert_to_rpn(self,expr):
         length = len(expr)
         if length < 2 or expr.isdigit():
             return expr
 
-        if self.__find_brackets(expr) == length-1:
+        if self.find_extra_brackets(expr):
             expr = expr[1:-1]
         
-        return self.__find_add_sub(self,expr) or self.__find_mul(self,expr) or expr
+        return self.find_add_sub(self,expr) or self.find_mul(self,expr) or expr
 
-        # 不要な括弧の検出
-    def __find_brackets(expr):
-        deep = 0
-        for ct, c in enumerate(expr):
+    def find_extra_brackets(expr):
+        brackets_deep = 0
+        for c in expr[:-1]:
             if c == '(':
-                deep += 1
+                brackets_deep += 1
             elif c == ')':
-                deep -= 1
+                brackets_deep -= 1
 
-            if deep == 0:
-                break
-        return ct
+            if brackets_deep == 0:
+                return False
+        return True
 
-        # 演算子の検出　＋、ー
-    def __find_add_sub(self,expr):
+    def find_add_sub(self,expr):
         if len(expr) < 2 or expr.isdigit():
             return None
-        result = []
-        code = []
-        code_ct = 0
-        deep = 0
+        output = []
+        code_stack = []
+        code_insert_timing = 0  #'*'を挿入するタイミングを管理する
+        brackets_deep = 0
         st = 0
         for ct, c in enumerate(expr):
-            deep = self.__deep_process(deep,c)
-            if deep == 0:
-                if c == '+' or c == '-':
-                    result.append(self.convert_to_rpn(self,expr[st:ct]))
-                    code.append(c)
-                    code_ct += 1
-                    if code_ct >= 2:
-                        result.append(code[0])
-                        code.pop(0)
-                        code_ct = 1
-                    st = ct + 1
+            brackets_deep = self.measure_brackets_deep(brackets_deep,c)
+            if brackets_deep > 0:
+                continue
+
+            if c == '+' or c == '-':
+                output.append(self.convert_to_rpn(self,expr[st:ct]))
+                code_stack.append(c)
+                code_insert_timing += 1
+                if code_insert_timing >= 2:
+                    output.append(code_stack[0])
+                    code_stack.pop(0)
+                    code_insert_timing = 1
+                st = ct + 1
         
-        if len(result) < 1:
+        if len(output) < 1:
             return None
 
-        result.append(self.convert_to_rpn(self,expr[st:]))
-        result.append(code[-1])
-        return result
+        output.append(self.convert_to_rpn(self,expr[st:]))
+        output.append(code_stack[-1])
+        return output
 
-        # 演算子の検出
-    def __find_mul(self,expr):
+    def find_mul(self,expr):
         if len(expr) < 2 or expr.isdigit():
             return None
-        result = []
-        code_ct = 0
+        output = []
+        code_insert_timing = 0  # ２になると演算子挿入
         deep = 0
         st = 0
         for ct, c in enumerate(expr):
-            deep = self.__deep_process(deep,c)
+            deep = self.measure_brackets_deep(deep,c)
             if deep == 0 and c != '-':
                 if c == '*':
                     st = ct + 1
 
                 elif ct >= len(expr)-1:
-                    result.append(self.convert_to_rpn(self,expr[st:]))
-                    code_ct += 1
+                    output.append(self.convert_to_rpn(self,expr[st:]))
+                    code_insert_timing += 1
                     st = ct + 1
 
                 elif ( c.isdecimal() and expr[ct+1].isdecimal()) == False:
-                    result.append(self.convert_to_rpn(self,expr[st:ct+1]))
-                    code_ct += 1
+                    output.append(self.convert_to_rpn(self,expr[st:ct+1]))
+                    code_insert_timing += 1
                     st = ct + 1
             
-                if( code_ct > 1 ):
-                    result.append('*')
-                    code_ct = 1
-        return result
+                if( code_insert_timing == 2 ):
+                    output.append('*')
+                    code_insert_timing = 1
+        return output
 
-        # 再帰関数の深さ
-    def __deep_process(deep,c):
+    def measure_brackets_deep(brackets_deep,c):
         if c == '(':
-            deep += 1
+            brackets_deep += 1
         elif c == ')':
-            deep -= 1
-        return deep
+            brackets_deep -= 1
+        return brackets_deep
+
+    def convert_list_to_str(self,list):
+        array = []
+        for cell in list:
+            if isinstance(cell, str):
+                array.append(cell)
+            else:
+                array += self.convert_list_to_str(self,cell)
+
+        return array
