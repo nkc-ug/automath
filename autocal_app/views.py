b977e063b9efde9165924e17932b973c688c8443
Iyarr <8.24ridiculous@gmail.com>
2022-12-07 02:58:02 +0000

    View.py urls.pyの設定

--- autocal_app/views.py
+++ autocal_app/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(repuest):
     return HttpResponse("Hello, world. You're at the autocal_app index.")
+
+def calculate(repuest,expr):
+    response = "You typed " + expr + "."
+    return HttpResponse(response)
